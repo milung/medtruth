7123272d14cdade77b2eadfee17d8e807c9ccc86
adrianplavka <adrian.plavka@gmail.com>
2017-07-17 15:41:20 +0100

    Added extension checker with tests

--- client/src/components/FileForm/FileForm.test.tsx
+++ client/src/components/FileForm/FileForm.test.tsx
@@ -7,4 +7,30 @@ describe('<FileForm />', () => {
     it('should render', () => {
         shallow(<FileForm />);
     });
+
+    it('valid file extension', () => {
+        // Given
+        const fileForm = shallow(<FileForm />);
+        const input = fileForm.getNode().props.children[0];
+
+        // When
+        const validName: string = 'valid.dcm';
+        const event: object = {target: {files: [{name: validName}]}}
+        
+        // Then
+        expect(input.props.onChange(event)).toBeTruthy();
+    });
+
+    it('invalid file extension', () => {
+        // Given
+        const fileForm = shallow(<FileForm />);
+        const input = fileForm.getNode().props.children[0];
+
+        // When
+        const invalidName: string = 'invalid.jpg';
+        const event: object = {target: {files: [{name: invalidName}]}}
+        
+        // Then
+        expect(input.props.onChange(event)).toBeFalsy();
+    })
 });
