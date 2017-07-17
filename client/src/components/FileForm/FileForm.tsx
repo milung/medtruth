f18d56c6e9ec02c3ddbb95137036113fad90c986
adrianplavka <adrian.plavka@gmail.com>
2017-07-17 14:54:44 +0200

    FileForm tests

--- client/src/components/FileForm/FileForm.tsx
+++ client/src/components/FileForm/FileForm.tsx
@@ -2,15 +2,20 @@
 import * as React from 'react';
 
 export default class FileForm extends React.Component<{}, {}> {
-    onChange(event: Event) {
+    constructor(props: React.Props<{}>) {
+        super();
+        this.onChange = this.onChange.bind(this);
+    }
 
+    onChange(event: any) {
+        
     }
 
     render() {
         return (
             <form>
                 <input type="file" />
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit" onChange={this.onChange}/>
             </form>
         );
     }
