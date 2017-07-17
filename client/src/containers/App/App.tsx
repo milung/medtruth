c237d34a3d7e94c4503dd5b50da2a4cb2617931c
adrianplavka <adrian.plavka@gmail.com>
2017-07-17 18:52:59 +0100

    Proper .gitignore

---
+++ client/src/containers/App/App.tsx
@@ -0,0 +1,11 @@
+
+import * as React from 'react';
+import FileForm from '../components/FileForm/FileForm';
+
+export default class App extends React.Component<{}, {}> {
+    render() {
+        return (
+            <FileForm />
+        ); 
+    }
+}
