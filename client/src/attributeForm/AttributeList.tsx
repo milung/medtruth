18c4b835ae799824d22062a60a384228a590f842
martina <durnekovamartina@gmail.com>
2017-08-02 10:24:22 +0200

    Change AttributeList

--- client/src/attributeForm/AttributeList.tsx
+++ client/src/attributeForm/AttributeList.tsx
@@ -2,25 +2,7 @@ import * as React from 'react';
 //import { TextField } from 'C:/Users/USER/Desktop/31.07.2017/SDA-GroundTruth/client/src/attributeForm/TextFields.tsx';
 import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
 // import Paper from 'material-ui/Paper';
-let id = 0;
-function createData(name, calories) {
-    id += 1;
-    return { id, name, calories };
-}
-const data = [
-    createData('Frozen yoghurt', 159),
-    createData('Ice cream sandwich', 237),
-    createData('Eclair', 262),
-    createData('Cupcake', 305),
-    createData('Gingerbread', 356),
-    createData('Frozen yoghurt', 159),
-    createData('Ice cream sandwich', 237),
-    createData('Eclair', 262),
-    createData('Cupcake', 305),
-    createData('Gingerbread', 356),
-    createData('Frozen yoghurt', 159),
-    createData('Ice cream sandwich', 2373),
-];
+
 interface listItem {
     atribute: string;
     value: number;
@@ -35,36 +17,33 @@ export class AttributeList extends React.Component<listOfAttributes, {}> {
         // this.props.listItems=props.listItems;
         console.log(this.props.listItems[2])
     }
-    public createData(name, calories) {
-        id += 1;
-        return { id, name, calories };
-    }
+
     render() {
         return (
             <div>
                 {/* <Paper> */}
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Dessert </TableCell>
-                                <TableCell numeric>Calories</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {data.map(n => {
-                                return (
-                                    <TableRow key={n.id}>
-                                        <TableCell>
-                                            {n.name}
-                                        </TableCell>
-                                        <TableCell numeric>
-                                            {n.calories}
-                                        </TableCell>
-                                    </TableRow>
-                                );
-                            })}
-                        </TableBody>
-                    </Table>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Dessert </TableCell>
+                            <TableCell numeric>Calories</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.props.listItems.map((item, i) => {
+                            return (
+                                <TableRow key={i}>
+                                    <TableCell>
+                                        {item.atribute}
+                                    </TableCell>
+                                    <TableCell numeric>
+                                        {item.value}
+                                    </TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
                 {/* </Paper> */}
             </div>
         );
