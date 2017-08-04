8aa64edfee631b3d14aedd8907fbaf1e9f62c514
Kristian Chudjak <Chudjakk@gmail.com>
2017-08-04 13:30:12 +0200

    for deploy

--- server/azure-service.ts
+++ server/azure-service.ts
@@ -64,10 +64,7 @@ export namespace AzureStorage {
 export namespace AzureDatabase {
     export const localAddress = "localhost:27017/";
     export const localName = "medtruth";
-
-    export const url = process.argv[2] === 'production'
-    ? "mongodb://medtruthdb:5j67JxnnNB3DmufIoR1didzpMjl13chVC8CRUHSlNLguTLMlB616CxbPOa6cvuv5vHvi6qOquK3KHlaSRuNlpg==@medtruthdb.documents.azure.com:10255/?ssl=true"
-    : "mongodb://" + localAddress + localName;
+    export const url = "mongodb://medtruthdb:5j67JxnnNB3DmufIoR1didzpMjl13chVC8CRUHSlNLguTLMlB616CxbPOa6cvuv5vHvi6qOquK3KHlaSRuNlpg==@medtruthdb.documents.azure.com:10255/?ssl=true";
 
     export enum Status {
         SUCCESFUL,
--- server/server.ts
+++ server/server.ts
@@ -23,9 +23,7 @@ server.use((req, res, next) => {
 })
 
 // Listen and serve.
-const port = process.argv[2] === 'production'
-            ? 80
-            : 8080;
-server.listen(port, () => {
+const port = 8080;
+server.listen(process.env.PORT || port, () => {
     console.log("Listening on port", port);
 });
